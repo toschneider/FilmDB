9219fab918f1aade1e87ff7183f89f0508cccdb3
toschneider <toschneider@uni-osnabrueck.de>
2019-12-18 17:24:33 +0100

    adds MovieViewModel Controller and IndexView

---
+++ FilmDB/Controllers/MovieViewModelController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FilmDB.Models;
+using FilmDB.DAL;
+
+namespace FilmDB.Controllers
+{
+    public class MovieViewModelController : Controller
+    {
+        private MovieContext dv = new MovieContext();
+        // GET: MovieViewModel
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
