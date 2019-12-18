9b1af277d2f098b7b099ab2cc2254d7d6726616a
toschneider <toschneider@uni-osnabrueck.de>
2019-12-18 12:43:15 +0100

    adds DbSets to MovieContext

--- FilmDB/DAL/MovieContext.cs
+++ FilmDB/DAL/MovieContext.cs
@@ -12,7 +12,22 @@ namespace FilmDB.DAL
 	{
 		public MovieContext() : base("MovieContext")
 		{
+		}
+		public DbSet<Actor> Actors { get; set; }
+		public DbSet<Director> Directors { get; set; }
+		public DbSet<Genre> Genres { get; set; }
+		public DbSet<Movie> Movies { get; set; }
+		public DbSet<MovieCast> MovieCasts { get; set; }
+		public DbSet<MovieCastRole> MovieCastRoles { get; set; }
+		public DbSet<MovieDirection> MovieDirections { get; set; }
+		public DbSet<MovieGenre> MovieGenres { get; set; }
+		public DbSet<Rating> Ratings { get; set; }
+		public DbSet<Reviewer> Reviewers { get; set; }
+		public DbSet<Role> Roles { get; set; }
 
+		protected override void OnModelCreating(DbModelBuilder modelBuilder)
+		{
+			modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
 		}
 	}
 }
