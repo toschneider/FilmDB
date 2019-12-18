a23ed61a6fa131363b36aea9fa6b35de1bc0bb0f
toschneider <toschneider@uni-osnabrueck.de>
2019-12-18 12:38:03 +0100

    creates MovieContext

---
+++ FilmDB/DAL/MovieContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using FilmDB.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace FilmDB.DAL
+{
+	public class MovieContext : DbContext
+	{
+		public MovieContext() : base("MovieContext")
+		{
+
+		}
+	}
+}
